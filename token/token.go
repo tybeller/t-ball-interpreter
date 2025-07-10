b300b8c7cb4c7cb0b756b6bae5b1fed1da18cd0b
Ty Beller <ty.c.beller@gmail.com>
2025-07-10 13:54:27 -0400

    initial tokenizer

---
+++ token/token.go
@@ -0,0 +1,34 @@
+package token
+
+type TokenType string
+
+type Token struct {
+	Type    TokenType
+	Literal string
+}
+
+const (
+	ILLEGAL = "ILLEGAL"
+	EOF     = "EOF"
+
+	// Identifiers and literals
+	IDENT = "IDENT" // add, sub, x, y, whatever
+	INT   = "INT"   // You know what an int is
+
+	// Ops
+	ASSIGN = "="
+	PLUS   = "+"
+
+	// Delims
+	COMMA     = ","
+	SEMICOLON = ";"
+
+	LPAREN = "("
+	RPAREN = ")"
+	LBRACE = "{"
+	RBRACE = "}"
+
+	// Keywords
+	FUNCTION = "FUNCTION"
+	LET      = "LET"
+)
